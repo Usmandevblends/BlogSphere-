aa90db4051163f913325b48efe80448e06f054ba
Usmandevblends <167763547+Usmandevblends@users.noreply.github.com>
2024-09-12 12:59:20 +0500

    Added Footer component and made some changes to Home component and output.css

--- blog/src/components/Footer.js
+++ blog/src/components/Footer.js
@@ -3,7 +3,49 @@ import React from 'react'
 function Footer() {
   return (
     <>
-        
+         <footer class="bg-[#805aed] py-8 mt-20">
+        <div class="container mx-auto px-4">
+            <div class="grid grid-cols-4 gap-8">
+                <div>
+                    <h3 class="text-lg font-semibold mb-4 text-white">Company</h3>
+                    <ul>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">About Us</a></li>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">Our Services</a></li>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">Privacy Policy</a></li>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">Affiliate Program</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h3 class="text-lg font-semibold mb-4 text-white">Get Help</h3>
+                    <ul>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">FAQ</a></li>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">Shipping</a></li>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">Returns</a></li>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">Order Status</a></li>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">Payment Options</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h3 class="text-lg font-semibold mb-4 text-white">Online Shop</h3>
+                    <ul>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">Watch</a></li>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">Bag</a></li>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">Shoes</a></li>
+                        <li class="mb-2"><a href="#" class="hover:underline text-white">Dress</a></li>
+                    </ul>
+                </div>
+                <div> 
+                    <h3 class="text-lg font-semibold mb-4 text-white">Follow Us</h3>
+                    <div class="flex space-x-4">
+                        <a href="#" class="text-white hover:text-white"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="text-white hover:text-white"><i class="fab fa-twitter"></i></a>
+                        <a href="#" class="text-white hover:text-white"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="text-white hover:text-white"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
     </>
   )
 }
--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Blog from "./Blog";
 import Banner from "./Banner";
 import TechReview from "./TechReview";
+import Footer from "./Footer";
 
 function Home({ articles }) {
   return (
@@ -9,6 +10,7 @@ function Home({ articles }) {
       <Banner articles={articles} />
       <Blog />
       <TechReview />
+      <Footer />
     </>
   );
 }
