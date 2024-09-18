3ef030d7bf5e208f09cda79c7d8a2379be5711d6
Usmandevblends <167763547+Usmandevblends@users.noreply.github.com>
2024-09-18 19:08:57 +0500

    Updated package dependencies and modified Gagetsblog component

--- blog/src/components/Gagetsblog.js
+++ blog/src/components/Gagetsblog.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 
 function Gagetsblog() {
+  const [isPlaying, setIsPlaying] = useState(0);
+  const ref = useRef(null)
+
+
+
+  const handleonclick = () => {
+    const nextIsPlaying = !isPlaying;
+    setIsPlaying(nextIsPlaying);
+
+    if(nextIsPlaying) {
+      ref.current.play();
+    }else {
+      ref.current.pause();
+    }
+
+   };
   return (
-    <section className="gblog-sec">
+    <section className="gblog-sec mb-5">
       <div className="container mx-auto">
-        <button className=' bg-slate-600 text-white border p-4 '> clicked me!</button>
+          <button
+            onClick={handleonclick}
+            className="bg-slate-600 text-white border p-2 mt-5  mb-s">{isPlaying ? 'Pause' : 'Play'}</button>
+          <video
+            width="250"
+            ref={ref}
+            onPlay={() => setIsPlaying(true)}
+            onPause={() => setIsPlaying(false)}>
+            <source
+              src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+              type="video/mp4"/>
+          </video>
+          <br></br>
       </div>
     </section>
   );
-}  
+}
 
 export default Gagetsblog;
-
-
